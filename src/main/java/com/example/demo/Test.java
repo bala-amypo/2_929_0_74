1002fe037e1a48f2b43b548c27fb29eb710844d9
bala-amypo <bala@amypo.in>
2025-12-17 18:41:30 +0000

    User commit

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -3,7 +3,5 @@ public class Test
 { 
 	public static void main(String[] args) { 
 		List<String> items = new ArrayList<>(); // ^ You should see red underline and lightbulb icon // Click the lightbulb or press Ctrl+. to add imports 
-		} 
-		ewnkjnnf
-		
+		}
 	}
